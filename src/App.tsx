e4db59a878e8ed2634cd13d0eab984c33cd86f20
dimitrykatana <dimitrykatana@gmail.com>
2023-01-23 16:48:52 +0100

    next step making collision

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const Snake = () => {
   const styleSerpent = { width: size[0], height: size[1], backgroundColor: 'red' };
   const [direction, setDirection] = useState<string>('');
   const [position, setPosition] = useState<[number, number]>([0, 0]);
-
+  
   const NewDirection = useCallback((event: KeyboardEvent) => {
     if (event.key === 'ArrowRight') {
       setDirection('right');
@@ -22,7 +22,7 @@ const Snake = () => {
       setDirection('down');
     }
   },[])
-
+  
   const handleKeyDown = () => {
     if (direction === 'right') {
       setPosition(([x, y]) => [x + 10, y])
@@ -34,35 +34,32 @@ const Snake = () => {
       setPosition(([x, y]) => [x, y - 10]);
     }
     if (direction === 'down') {
+      console.log(window.innerHeight)
       setPosition(([x, y]) => [x, y + 10]);
     }
   };
   
   const [intervalId, setIntervalId] = useState<number>();
-
   useEffect(() => {
-    setIntervalId(setInterval(handleKeyDown, 100));
-    clearInterval(intervalId);
-
-    const handleKeyDownEvent = (event: KeyboardEvent) => NewDirection(event);
-    document.addEventListener('keydown', handleKeyDownEvent);
+    setIntervalId(setInterval(handleKeyDown, 80));
+    document.addEventListener('keydown', NewDirection);
+    return (clearInterval(intervalId));
   }, [direction]);
 
   return (
     <div
-      style={{ ...styleSerpent, position: 'absolute', left: position[0], top: position[1] }}
+    style={{ ...styleSerpent, position: 'absolute', left: position[0], top: position[1] }}
     >
-      
     </div>
-  );
-}
-
+    );
+  }
+  
 function App(): JSX.Element {
   return (
     <div className="App">
-      <Snake />
+    <Snake />
     </div>
   );
 }
-
+  
 export default App;
