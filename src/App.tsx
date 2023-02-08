96d773d663d851baa16c65523cf18b8817dfcd9a
dimitrykatana <dimitrykatana@gmail.com>
2023-02-08 09:55:40 +0100

    pokemon-list

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,36 @@
 import React, { useState, useEffect } from 'react';
-import ManageList from './ManageList';
+import axios from 'axios';
+
 
 const Form = (): JSX.Element => {
   const [inputValue, setInputValue] = useState('')
-  
   const [list, setList] = useState<string[]>([]);
   
+  
+useEffect(() => {
+    axios.get(`https://pokeapi.co/api/v2/generation/1/`)
+    .then(res => {
+      const Les_pokemonz = res.data.pokemon_species.map((pokemonz : { name: string}) => pokemonz.name)
+      setList( Les_pokemonz)
+    })
+  
+    .catch(error => console.error(error));
+  }, []);
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    // permet d'éviter le rechargement de la page
     event.preventDefault();
+    // Ajoute le inputvalue à la liste, donc ce qui est tapé dans le formulaire est dans liste
     setList([...list, inputValue]);
+    // on réinitialise la valeur de inputvalue, donc on revide le formmulaire
     setInputValue('');
   };
-  const handleDelete = (index: number) => {
-    let newList = [...list]
-    newList.splice(index, 1)
-    setList(newList)
-  }
-  
   
   return (
     <>
     <h1> To do list</h1>
     <form onSubmit={handleSubmit}>
-    <label>Enter your name:
+    <label>type the number of the pokemon:
     <input type="text" 
     value={inputValue}
     onChange={(e) => setInputValue(e.target.value)}/>
@@ -33,10 +41,9 @@ const Form = (): JSX.Element => {
     
     <ol>
     {list.map((item, index) => (
-      <li key={index}>{item} <button onClick={()=> handleDelete(index)} >delete</button></li>
+      <li key={index}>{item}</li>
       ))}
       </ol>
-      
       </>
       )
     }
