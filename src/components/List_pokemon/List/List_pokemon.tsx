36f070fab2f643d268609c64cd69d14dc57978b7
dimitrykatana <dimitrykatana@gmail.com>
2023-03-13 00:12:16 +0100

    refactored js, todo: refactor css

--- src/components/List_pokemon/List/List_pokemon.tsx
+++ src/components/List_pokemon/List/List_pokemon.tsx
@@ -1,46 +1,66 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import './List_pokemon.css';
-import {Lien_image} from '../../API_CO/api';
+import { Lien_image } from '../../API_CO/api';
 
 type Props = {
-    filtrage: string;
+  filtrage: string;
 };
 
-const Liste = (props : Props): JSX.Element => {
-    const [list, setList] = useState<string[]>([]); 
-    const [opacity, setOpacity] = useState(0);
-    
-    useEffect(() => {
-      axios.get(`https://pokeapi.co/api/v2/pokemon?limit=151`)
-      .then(res => {
-        // list the names of all poke monsterzzz.
-        const Les_pokemonz = res.data.results.map((pokemonz : { name: string}) => pokemonz.name)
-        // put them in the list
-        setList( Les_pokemonz)
+const Liste = (props: Props): JSX.Element => {
+  const [list, setList] = useState<string[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [opacity, setOpacity] = useState(0);
+  const animationDelay = 0.1; // in seconds
+
+  const filteredList = useCallback(() => {
+    return list.filter((item) =>
+      item.toLowerCase().includes(props.filtrage.toLowerCase())
+    );
+  }, [list, props.filtrage]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon?limit=151`)
+      .then((res) => {
+        setList(res.data.results.map((pokemonz: { name: string }) => pokemonz.name));
+        setIsLoading(false);
       })
-      .catch(error => console.error(error));
-    }, []);
-    const filteredList = list.filter(item =>
-        item.includes(props.filtrage.toLowerCase())
-    );
+      .catch((error) => {
+        console.error(error);
+        setIsLoading(false);
+      });
+  }, []);
 
-  const animationDelay = 0.2; // in seconds
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (list.length === 0) {
+    return <div>No results found</div>;
+  }
 
-    return(
-      <ul className='Pokemons'>
-      {filteredList.map((item, index) => (
-        <>
-        <li className='PokemonCard' key={item} onLoad={() => setOpacity(1)} style={{ opacity:opacity, transition: `opacity 1s ease-in ${index*animationDelay}s ` } }>
-          <div className='HeadCard'>
-            <p className='Nom'>{item}</p></div>
-            <img className='image' src={Lien_image(list.indexOf(item))} alt={`${item}`} />
-            <p className='Numero'>No. {list.indexOf(item)+1}</p>
+  return (
+    <ul className="Pokemons">
+      {filteredList().map((item) => (
+        <li
+          className="PokemonCard"
+          key={item}
+          onLoad={() => setOpacity(1)}
+          style={{
+            opacity: opacity,
+            transition: `opacity 1s ease-in ${list.indexOf(item) * animationDelay}s`,
+          }}
+        >
+          <div className="HeadCard">
+            <p className="Nom">{item}</p>
+          </div>
+          <img className="image" src={Lien_image(list.indexOf(item))} alt={`${item}`} />
+          <p className="Numero">No. {list.indexOf(item) + 1}</p>
         </li>
-        </>
       ))}
-      </ul>
-    );
-}
+    </ul>
+  );
+};
 
 export default Liste;
